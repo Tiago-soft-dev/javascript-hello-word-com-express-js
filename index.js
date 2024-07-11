334df3e9f392f1a968c5f5cd31b31f2daed354fd
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-07-11 18:35:56 -0300

    adição do endpoint '/oi' e criação do script 'start'

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express=require('express')
 const app=express()
 
 app.get('/',(req,res)=>{
-    res.send('hello word')
+    res.send('Hello Word')
+})
+
+app.get('/oi', (req,res)=>{
+    res.send('Olá, Mundo!')
 })
 
 
